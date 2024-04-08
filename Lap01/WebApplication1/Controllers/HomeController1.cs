de704509bc0c2306024984cd7154cdf759f63815
Hoang-Kim-Oanh <hoangkimoanh04052004@gmail.com>
2024-04-08 23:36:53 +0700

    Lap1.1

---
+++ Lap01/WebApplication1/Controllers/HomeController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
